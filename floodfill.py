796b7eb25f8f2fecf866e6322159256b3db6137d
jinchenglee <jincheng.li@gmail.com>
2016-07-31 22:57:49 -0700

    Add golden_path/tmp_path variables to track fastest path from start to destination point.

--- floodfill.py
+++ floodfill.py
@@ -1,6 +1,9 @@
 import numpy as np
+from copy import deepcopy
 
-def fill(cur_distance=0, cur_pos=(0,0), tgt_pos=(1,1), obstacles=[], path_map=[], best_distance=255): 
+golden_path = []
+
+def fill(cur_distance=0, cur_pos=(0,0), tgt_pos=(1,1), obstacles=[], path_map=[], tmp_best_distance=255, tmp_path=[]): 
     """
     Shortest path finding using flood-fill algorithm. Recursive calling, distance 
     increments 1 for every call. Path_map records the distances for every position.
@@ -11,12 +14,18 @@ def fill(cur_distance=0, cur_pos=(0,0), tgt_pos=(1,1), obstacles=[], path_map=[]
     cur_pos: Tuple of coordinates (x,y) of current standpoint.
     tgt_pos: Tuple of coordinates of target position.
     obstacles: A list of coordinates tuples of obstacles that path has to get around. 
+    path_map: A MxN array to hold distance from each (x,y) position to starting point.
+    tmp_best_distance: Current the best distance during searching. Passing between recursive calls.
+    tmp_path: Current shortest path to target. Passing between recursive calls.
 
     Returns:
     ---------
-    Shortest distance till current call. 
-    In the meantime, ``path_map`` records are updated lively.
+    distance: Shortest distance till current call. 
+    golden_path: Final shortest path after all searches. 
     """
+
+    global golden_path
+
     # Get boundaries of search
     M,N = path_map.shape
     # Pos
@@ -30,53 +39,75 @@ def fill(cur_distance=0, cur_pos=(0,0), tgt_pos=(1,1), obstacles=[], path_map=[]
     # Distance update
     nxt_distance = cur_distance + 1
     # Debug print
-    print("(x,y)=(",x,",", y,") cur_distance =", cur_distance, "best_distance =", best_distance)
+    #print("(x,y)=(",x,",", y,") cur_distance =", cur_distance, "best_distance =", tmp_best_distance)
 
     # At the target already!
     if (cur_pos == tgt_pos):
         print("At the target! Best_distance = ", cur_distance)
-        print(path_map)
-        return cur_distance
+        golden_path = deepcopy(tmp_path)
+        #print(path_map)
+        #print(golden_path)
+        return cur_distance,golden_path
 
+    if tmp_best_distance > nxt_distance:
 
-    if best_distance > nxt_distance:
         # West
         if x-1 >= 0:
-            if path_map[x-1,y]>nxt_distance:
-                best_distance = fill(nxt_distance, (x-1,y), tgt_pos, obstacles, path_map, best_distance)
-        else:
-            print("Hit wall! West side")
+            if path_map[x-1,y]>nxt_distance and ((x-1,y) not in obstacles):
+                tmp_path.append((x,y))
+                tmp_best_distance,golden_path = fill(nxt_distance, (x-1,y), tgt_pos, obstacles, path_map, tmp_best_distance, tmp_path)
+                tmp_path.pop()
+        #else:
+        #    print("Hit wall! West side")
+
         # East 
         if x+1 < M:
-            if path_map[x+1,y]>nxt_distance:
-                best_distance = fill(nxt_distance, (x+1,y), tgt_pos, obstacles, path_map, best_distance)
-        else:
-            print("Hit wall! East side")
+            if path_map[x+1,y]>nxt_distance and ((x+1,y) not in obstacles):
+                tmp_path.append((x,y))
+                tmp_best_distance,golden_path = fill(nxt_distance, (x+1,y), tgt_pos, obstacles, path_map, tmp_best_distance, tmp_path)
+                tmp_path.pop()
+        #else:
+        #    print("Hit wall! East side")
+
         # North
         if y-1 >= 0:
-            if path_map[x,y-1]>nxt_distance:
-                best_distance = fill(nxt_distance, (x,y-1), tgt_pos, obstacles, path_map, best_distance)
-        else:
-            print("Hit wall! North side")
+            if path_map[x,y-1]>nxt_distance and ((x,y-1) not in obstacles):
+                tmp_path.append((x,y))
+                tmp_best_distance,golden_path = fill(nxt_distance, (x,y-1), tgt_pos, obstacles, path_map, tmp_best_distance, tmp_path)
+                tmp_path.pop()
+        #else:
+        #    print("Hit wall! North side")
+
         # South
         if y+1 < N:
-            if path_map[x,y+1]>nxt_distance:
-                best_distance = fill(nxt_distance, (x,y+1), tgt_pos, obstacles, path_map, best_distance)
-        else:
-            print("Hit wall! South side")
+            if path_map[x,y+1]>nxt_distance and ((x,y+1) not in obstacles):
+                tmp_path.append((x,y))
+                tmp_best_distance,golden_path = fill(nxt_distance, (x,y+1), tgt_pos, obstacles, path_map, tmp_best_distance, tmp_path)
+                tmp_path.pop()
+        #else:
+        #    print("Hit wall! South side")
 
-    return best_distance
+    return tmp_best_distance,golden_path
 
 
 # Boundary of search
-M,N = 10, 10
-start_pos = (2,8)
-tgt_pos = (9,0)
-obstacles=[]
+M,N = 10,9 
+start_pos = (2,1)
+tgt_pos = (7,8)
+obstacles=[(2,3),(2,4),(3,4),(4,4),(5,4),(6,4),(7,4),(8,4)]
 path_map = np.zeros((M,N)) + 255
+tmp_path = []
+distance = 255
 
-fill(0, start_pos, tgt_pos, obstacles, path_map, 255)
+distance,golden_path = fill(0, start_pos, tgt_pos, obstacles, path_map, 255, tmp_path)
 
 path_map[tgt_pos[0], tgt_pos[1]] = -1
 print("start (",start_pos[0], start_pos[1],"), end (", tgt_pos[0], tgt_pos[1], ")")
+print(path_map)
+print(golden_path)
+
+for pos in golden_path:
+    path_map[pos] = 111
+
+
 print(path_map)
